46e0e546581e45c216a686ab75205667b1d7740b
praful-prakash-goel <prafulgoel1000@gmail.com>
2026-03-23 19:16:12 +0530

    Added database schema

---
+++ pmas.sql
@@ -0,0 +1,196 @@
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: localhost    Database: pmas
+-- ------------------------------------------------------
+-- Server version	8.0.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alerts`
+--
+
+DROP TABLE IF EXISTS `alerts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alerts` (
+  `alert_id` int NOT NULL AUTO_INCREMENT,
+  `machine_id` varchar(50) DEFAULT NULL,
+  `severity` enum('LOW','MEDIUM','HIGH') DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `acknowledged` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`alert_id`),
+  KEY `machine_id` (`machine_id`),
+  CONSTRAINT `alerts_ibfk_1` FOREIGN KEY (`machine_id`) REFERENCES `machines` (`machine_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alerts`
+--
+
+LOCK TABLES `alerts` WRITE;
+/*!40000 ALTER TABLE `alerts` DISABLE KEYS */;
+INSERT INTO `alerts` VALUES (1,'M01','HIGH','2026-03-22 16:05:07',1),(2,'M02','LOW','2026-03-22 16:05:14',0);
+/*!40000 ALTER TABLE `alerts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `machines`
+--
+
+DROP TABLE IF EXISTS `machines`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `machines` (
+  `machine_id` varchar(50) NOT NULL,
+  `machine_type` varchar(100) DEFAULT NULL,
+  `health_status` enum('HEALTHY','DEGRADING','CRITICAL') DEFAULT NULL,
+  `installation_date` date DEFAULT NULL,
+  `last_service_date` date DEFAULT NULL,
+  `location` varchar(100) DEFAULT NULL,
+  `org_name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`machine_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `machines`
+--
+
+LOCK TABLES `machines` WRITE;
+/*!40000 ALTER TABLE `machines` DISABLE KEYS */;
+INSERT INTO `machines` VALUES ('M01','CNC Mill','HEALTHY','2026-03-21','2026-03-22','Warehouse A','Nexus'),('M02','Drill','HEALTHY','2026-03-22','2026-03-22','Production Line A','Nexus');
+/*!40000 ALTER TABLE `machines` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `technicians`
+--
+
+DROP TABLE IF EXISTS `technicians`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `technicians` (
+  `user_id` int NOT NULL,
+  `availability_status` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`user_id`),
+  CONSTRAINT `technicians_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `technicians`
+--
+
+LOCK TABLES `technicians` WRITE;
+/*!40000 ALTER TABLE `technicians` DISABLE KEYS */;
+INSERT INTO `technicians` VALUES (2,1),(3,1);
+/*!40000 ALTER TABLE `technicians` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ticket_technicians`
+--
+
+DROP TABLE IF EXISTS `ticket_technicians`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ticket_technicians` (
+  `ticket_id` int NOT NULL,
+  `technician_id` int NOT NULL,
+  `is_assigned` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`ticket_id`,`technician_id`),
+  KEY `technician_id` (`technician_id`),
+  CONSTRAINT `ticket_technicians_ibfk_1` FOREIGN KEY (`ticket_id`) REFERENCES `tickets` (`ticket_id`) ON DELETE CASCADE,
+  CONSTRAINT `ticket_technicians_ibfk_2` FOREIGN KEY (`technician_id`) REFERENCES `technicians` (`user_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ticket_technicians`
+--
+
+LOCK TABLES `ticket_technicians` WRITE;
+/*!40000 ALTER TABLE `ticket_technicians` DISABLE KEYS */;
+INSERT INTO `ticket_technicians` VALUES (1,2,1),(1,3,1);
+/*!40000 ALTER TABLE `ticket_technicians` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tickets`
+--
+
+DROP TABLE IF EXISTS `tickets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tickets` (
+  `ticket_id` int NOT NULL AUTO_INCREMENT,
+  `alert_id` int DEFAULT NULL,
+  `priority` enum('LOW','MEDIUM','HIGH') DEFAULT NULL,
+  `status` enum('OPEN','IN PROGRESS','RESOLVED') DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ticket_id`),
+  KEY `alert_id` (`alert_id`),
+  CONSTRAINT `tickets_ibfk_1` FOREIGN KEY (`alert_id`) REFERENCES `alerts` (`alert_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tickets`
+--
+
+LOCK TABLES `tickets` WRITE;
+/*!40000 ALTER TABLE `tickets` DISABLE KEYS */;
+INSERT INTO `tickets` VALUES (1,1,'HIGH','OPEN','2026-03-22 11:10:24'),(2,1,'HIGH','OPEN','2026-03-22 11:11:32'),(3,2,'LOW','OPEN','2026-03-22 11:11:44');
+/*!40000 ALTER TABLE `tickets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('ADMIN','TECHNICIAN') DEFAULT NULL,
+  `org_name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Almighty','almighty@gmail.com','$2b$12$UeAwCdDmBGC2D5hW1pivr.FSz13a1uOCcPcQoWhjMvVDqSe0vT7Qe','ADMIN','Nexus'),(2,'john doe','johndoe@gmail.com','$2b$12$zFyJ/JRbAQGFG8rqFzGjte7.Lm9SZQGXt3GUVVMRk24j94oAddHWi','TECHNICIAN','Nexus'),(3,'jane doe','janedoe@gmail.com','$2b$12$8kKwcGElYX4KvKbgwvzEAuC8juu29g0/tfzjlw2NajlluFqT/4xaO','TECHNICIAN','Nexus');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-03-23 19:15:52
